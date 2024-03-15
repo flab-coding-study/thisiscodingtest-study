f15cc050864a31441219386346d66ead5ca34573
jieun-dev1 <jkdev1324@gmail.com>
2024-03-15 23:27:53 +0900

    정확성 12,15,16 탈락에 대한 반례. 완전 탐색을 하게 되면, 이렇게 목적지가 1인 경우 2를 출력하게 된다. 따라서, (1,1)부터 연결된 원소만 큐에 담아야 한다.

--- DFS-BFS/게임맵최단거리/김지은/ShortestPath.py
+++ DFS-BFS/게임맵최단거리/김지은/ShortestPath.py
@@ -5,23 +5,27 @@ dy = [0, 1, 0, -1]
 
 def solution(maps):
     queue = deque()
-    for i in range(len(maps)):
-        for j in range(len(maps[0])):
-            if maps[i][j] == 1:
-                queue.append((i, j))
-                while queue:
-                    cur_x, cur_y = queue.popleft()
-                    for k in range(4):
-                        nx = cur_x + dx[k]
-                        ny = cur_y + dy[k]
-                        if nx == len(maps)-1 and ny == len(maps[0])-1:
-                            return maps[cur_x][cur_y]+1
-                        if 0 <= nx < len(maps) and 0 <= ny < len(maps[0]) and maps[nx][ny] == 1:
-                            queue.append((nx, ny))
-                            maps[nx][ny] = maps[cur_x][cur_y]+1
+    queue.append((0, 0))
+    while queue:
+        cur_x, cur_y = queue.popleft()
+        for k in range(4):
+            nx = cur_x + dx[k]
+            ny = cur_y + dy[k]
+            if nx == len(maps)-1 and ny == len(maps[0])-1:
+                return maps[cur_x][cur_y]+1
+            if 0 <= nx < len(maps) and 0 <= ny < len(maps[0]) and maps[nx][ny] == 1:
+                queue.append((nx, ny))
+                maps[nx][ny] = maps[cur_x][cur_y]+1
     return -1
 
-maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]
+#maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]
+maps = [
+    [1, 0, 1, 1, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0],
+    [0, 0, 0, 1, 1],
+]
 print(solution(maps))
 
 
